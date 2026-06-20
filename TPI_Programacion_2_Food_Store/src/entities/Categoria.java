a3c53f6d1c8a082922f963c78072a0de20bcf21f
Ramiro Quiroga <quirogarami11@gmail.com>
2026-06-20 16:51:49 -0300

    Subo clases Categoria y Producto

--- TPI_Programacion_2_Food_Store/src/entities/Categoria.java
+++ TPI_Programacion_2_Food_Store/src/entities/Categoria.java
@@ -1,9 +1,56 @@
 package entities;
-
 import java.util.ArrayList;
 
 public class Categoria extends Base {
     private String nombre;
     private String descripcion;
     private ArrayList<Producto> productos = new ArrayList<>();
+
+    public Categoria() {
+    }
+
+    public Categoria(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        if (nombre == null || nombre.trim().isEmpty()) {
+            System.out.println("Este campo es obligatorio, no puede estar vacío");
+        } else if (!nombre.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+")){
+            //Aseguro que el nombre solo contenga letras
+            System.out.println("Ingrese un nombre válido");
+        } else {
+            this.nombre = nombre.trim();
+        }
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        if (descripcion == null || descripcion.trim().isEmpty()) {
+            System.out.println("Este campo es obligatorio, no puede estar vacío");
+        } else {
+            this.descripcion = descripcion;
+        }
+    }
+
+    public ArrayList<Producto> getProductos() {
+        return productos;
+    }
+
+    public void agregarProducto(Producto producto) {
+        productos.add(producto);
+    }
+
+    public void eliminarProducto(Producto producto) {
+        productos.remove(producto);
+    }
+
 }
--- TPI_Programacion_2_Food_Store/src/entities/Producto.java
+++ TPI_Programacion_2_Food_Store/src/entities/Producto.java
@@ -7,4 +7,86 @@ public class Producto extends Base {
     private int stock;
     private String imagen;
     private boolean disponible;
+
+    public Producto() {
+    }
+
+    public Producto(String nombre, double precio, String descripcion, int stock, String imagen, boolean disponible) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.stock = stock;
+        this.imagen = imagen;
+        this.disponible = disponible;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        if (nombre == null || nombre.trim().isEmpty()) {
+            System.out.println("Este campo es obligatorio, no puede estar vacío");
+        } else if (!nombre.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+")) {
+            //Aseguro que el nombre solo contenga letras
+            System.out.println("Ingrese un nombre válido");
+        } else {
+            this.nombre = nombre.trim();
+        }
+
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        if (precio < 0) {
+            System.out.println("Ingrese un rango de precio correcto");
+        } else {
+            this.precio = precio;
+        }
+
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        if (descripcion == null || descripcion.trim().isEmpty()) {
+            System.out.println("Este campo es obligatorio, no puede estar vacío");
+        } else {
+            this.descripcion = descripcion;
+        }
+
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        if (stock < 0) {
+            System.out.println("Ingrese una cantidad de stock correcta");
+        } else {
+            this.stock = stock;
+        }
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
 }
